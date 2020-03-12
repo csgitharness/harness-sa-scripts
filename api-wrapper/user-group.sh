23a29ea3b5159f71c111669568121a65bafc71e0
Rohan Gupta <rohan.gupta@harness.io>
2020-03-11 18:08:19 -0700

    changes

--- api-wrapper/user-group.sh
+++ api-wrapper/user-group.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ## Variables
 EMAIL=""
 NAME=""
@@ -6,26 +7,36 @@ USERGROUPID=""
 USERGROUPNAME=""
 USERID=""
 APPNAME=""
+CURL_URL="https://app.harness.io/gateway/api/graphql?accountId=$HARNESS_ACCOUNT_ID"
+QUERY1='{"query":"{\n applications(limit: 5){\n  pageInfo{\n   total\n  }\n  nodes{\n   name\n  }\n }\n \n}","variables":{}}'
+
 # 1. Create a user group
 # 2. create a user
 # 3. add to a user group
 # 4. Add Permissions to that Group (limit it to application)
 # 5. Scope the application to the group, get the ID
 
+# '{"query":"{\n  userGroups(limit:20, offset:0){\n    nodes{\n      id\n      name\n    }\n  }\n}","variables":null}' --compressed > listusergroup.json
+# jq '.data.userGroups.nodes[] | select(.name=="'$USERGROUPNAME'")' listusergroup.json > group.json
+
 # Get the list of user groups
+fn_common_exc(){
+curl -s -X POST $CURL_URL \
+-H "x-api-key: $HARNESS_KEY" \
+-H 'Content-Type: application/json' \
+--data-binary @-
+}
+
+fn_get_user_group1(){
+echo "Fetch existing user groups"
+echo $QUERY1 | fn_common_exc > applist.json
+}
+
 fn_get_user_groups(){
 echo "Fetch existing user groups"
-curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
--H 'authority: app.harness.io' \
+curl $CURL_URL \
 -H 'x-api-key: '$HARNESS_KEY'' \
 -H 'content-type: application/json' \
--H 'accept: */*' \
--H 'origin: https://app.harness.io' \
--H 'sec-fetch-site: same-origin' \
--H 'sec-fetch-mode: cors' \
--H 'referer: https://app.harness.io/' \
--H 'accept-encoding: gzip, deflate, br' \
--H 'accept-language: en-US,en;q=0.9' \
 --data-binary '{"query":"{\n  userGroups(limit:20, offset:0){\n    nodes{\n      id\n      name\n    }\n  }\n}","variables":null}' --compressed > listusergroup.json
 jq '.data.userGroups.nodes[] | select(.name=="'$USERGROUPNAME'")' listusergroup.json > group.json
 USERGROUPID=$(cat group.json | jq -r '.id')
@@ -37,16 +48,8 @@ rm -rf group.json
 fn_create_user_group(){
 echo "****Creating a User Group****"
 curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
- -H 'authority: app.harness.io' \
  -H 'x-api-key: '$HARNESS_KEY'' \
- -H 'origin: https://app.harness.io' \
  -H 'content-type: application/json' \
- -H 'accept: */*' \
- -H 'sec-fetch-site: same-origin' \
- -H 'sec-fetch-mode: cors' \
- -H 'referer: https://app.harness.io/' \
- -H 'accept-encoding: gzip, deflate, br' \
- -H 'accept-language: en-US,en;q=0.9' \
  --data-binary $'{"query":"mutation($userGroup: CreateUserGroupInput\u0021){\\n  createUserGroup (input:$userGroup) {\\n    userGroup {\\n      id\\n      name\\n      description\\n      isSSOLinked\\n      importedByScim\\n      notificationSettings {\\n        sendNotificationToMembers\\n        sendMailToNewMembers\\n        slackNotificationSetting {\\n          slackChannelName\\n          slackWebhookURL\\n        }\\n        groupEmailAddresses\\n      }\\n    }\\n  }\\n}","variables":{"userGroup":{"name":"'$USERGROUPNAME'"}}}' --compressed > usergroup.json
 USERGROUPID=$(cat usergroup.json | jq -r '.data.createUserGroup.userGroup.id')
 echo $USERGROUPID
@@ -135,14 +138,20 @@ echo "ERROR: Not enough arguments"
 echo "<FLAG> <EMAIL> <USER_NAME> <USER_GROUP_NAME> <APPNAME>"
 exit 0
 fi 
-if [ "$1" != "-add" ]; then
+
+if [ "$1" == "-user" ]; then 
+fn_get_user_group1
+exit 0 
+fi
+
+if [ "$1" == "-add" ]; then
   fn_get_user_groups
   fn_create_user
   fn_update_user_group
   fn_get_apps
   fn_add_permissions
 fi
-if [ "$1" != "-create" ]; then
+if [ "$1" == "-create" ]; then
   fn_create_user_group
   fn_create_user
   fn_update_user_group
---
+++ curlCommands/curl.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Create Application
+APP_NAME="SampleApp"
+APP_DESCRIPTION="Created by Automation Script"
+
+fn_create_app(){
+echo "Creating an Application"
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+-H 'x-api-key: '$HARNESS_KEY'' \
+-H 'content-type: application/json' \
+--data-binary $'{"query":"mutation createapp($app: CreateApplicationInput\u0021) {\\n  createApplication(input: $app) {\\n    clientMutationId\\n    application {\\n      name\\n      id\\n    }\\n  }\\n}","variables":{"app":{"clientMutationId":"req9","name":"'$APP_NAME'","description":"'$APP_DESCRIPTION'"}},"operationName":"createapp"}' --compressed
+}
+
+
+
+# Delete Application 
+APP_ID="uP22_SFwTHe_-qYwzkm9mA"
+fn_delete_app(){
+echo "Deleting Application" 
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+-H 'x-api-key: '$HARNESS_KEY'' \
+-H 'content-type: application/json' \
+ --data-binary $'{"query":"mutation deleteapp($app: DeleteApplicationInput\u0021) {\\n  deleteApplication(input: $app){\\n    clientMutationId\\n  }\\n}","variables":{"app":{"applicationId":"'$APP_ID'"}},"operationName":"deleteapp"}' --compressed
+}
+
+
+# Create User Group
+USERGROUPNAME="SampleGroup"
+fn_create_user_group(){
+echo "****Creating a User Group****"
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+ -H 'x-api-key: '$HARNESS_KEY'' \
+ -H 'content-type: application/json' \
+ --data-binary $'{"query":"mutation($userGroup: CreateUserGroupInput\u0021){\\n  createUserGroup (input:$userGroup) {\\n    userGroup {\\n      id\\n      name\\n      description\\n      isSSOLinked\\n      importedByScim\\n      notificationSettings {\\n        sendNotificationToMembers\\n        sendMailToNewMembers\\n        slackNotificationSetting {\\n          slackChannelName\\n          slackWebhookURL\\n        }\\n        groupEmailAddresses\\n      }\\n    }\\n  }\\n}","variables":{"userGroup":{"name":"'$USERGROUPNAME'"}}}' --compressed 
+}
+
+# Delete User Group 
+USERGROUPID="fqn3QnvYSAOjhAMkc7YxIw"
+fn_delete_user_group(){
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+ -H 'x-api-key: '$HARNESS_KEY'' \
+ -H 'content-type: application/json' \
+--data-binary $'{"query":"mutation($userGroup: DeleteUserGroupInput\u0021){\\n  deleteUserGroup (input:$userGroup) {\\n\\t\\tclientMutationId\\n  }\\n}","variables":{"userGroup":{"userGroupId":"'$USERGROUPID'"}}}' --compressed
+}
+
+
+# Create a User
+USEREMAIL="sample@harness.io"
+USERNAME="captainCanary"
+
+fn_create_user(){
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+-H 'x-api-key: '$HARNESS_KEY'' \
+-H 'content-type: application/json' \
+--data-binary $'{"query":"mutation($user: CreateUserInput\u0021){\\n  createUser (input:$user) {\\n\\t\\tclientMutationId\\n  }\\n}","variables":{"user":{"email":"'$USEREMAIL'","name":"'$USERNAME'"}}}' --compressed
+}
+
+
+# Update UserGroup 
+USERID="fsyzR55DSPuz7UG1lki3eQ"
+USERGROUPID="-c7ZpUokQBCKBXnmiaTY0g"
+fn_update_user_group(){
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+-H 'content-type: application/json' \
+-H 'x-api-key: '$HARNESS_KEY'' \
+--data-binary $'{"query":"mutation($user: UpdateUserGroupInput\u0021){\\n  updateUserGroup(input: $user){\\n    clientMutationId\\n    userGroup{\\n      name\\n      id\\n    }\\n  }\\n}","variables":{"user":{"userIds":"'$USERID'","userGroupId":"'$USERGROUPID'"}}}' --compressed
+}
+
+
+# Add permissions to the group
+USERGROUPID="fqn3QnvYSAOjhAMkc7YxIw"
+APP_ID="uP22_SFwTHe_-qYwzkm9mA"
+fn_add_permissions(){
+echo "*****adding permissions to the group****"
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+ -H 'x-api-key: '$HARNESS_KEY'' \
+ -H 'content-type: application/json' \
+--data-binary $'{"query":"mutation($userGroup: UpdateUserGroupPermissionsInput\u0021){\\n  updateUserGroupPermissions (input:$userGroup) {\\n      permissions {\\n        accountPermissions{\\n          accountPermissionTypes\\n        }\\n        appPermissions {\\n          permissionType\\n          applications {\\n            filterType\\n            appIds\\n          }\\n          applications{\\n          filterType\\n            appIds\\n          }\\n        }\\n      }\\n  }\\n}\\n\\n","variables":{"userGroup":{"userGroupId":"'$USERGROUPID'","permissions":{"accountPermissions":{"accountPermissionTypes":["READ_USERS_AND_GROUPS","MANAGE_TEMPLATE_LIBRARY","VIEW_AUDITS"]},"appPermissions":[{"permissionType":"ALL","applications":{"appIds":["'$APPID'"]},"actions":["CREATE","READ","UPDATE"]}]}}}}' --compressed
+}
+
+
+# Fetch User By Name
+USERNAME="captainCanary"
+fn_fetch_user_by_name(){
+curl 'https://app.harness.io/gateway/api/graphql?accountId='$HARNESS_ACCOUNT_ID'' \
+-H 'x-api-key: '$HARNESS_KEY'' \
+-H 'content-type: application/json' \
+--data-binary '{"query":"{\n  userByName(name:\"'$USERNAME'\"){\n    id\n    name\n    email\n  }\n}","variables":null}' --compressed
+}
+
+
